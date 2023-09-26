850a08a341bde13d0cb95f1c81c985ccfe275e04
mikwan <nanana0909@gmail.com>
2023-09-26 13:55:24 +0900

    git commit

--- app/src/main/java/kr/hs/smc/app01_memo/MainActivity.kt
+++ app/src/main/java/kr/hs/smc/app01_memo/MainActivity.kt
@@ -4,13 +4,23 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.SystemClock
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import kr.hs.smc.app01_memo.databinding.ActivityMainBinding
+import kr.hs.smc.app01_memo.databinding.MainRecyclerRowBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+
+    //제목을 담을 ArrayList
+    val subject_list = ArrayList<String>()
+    //작성 날짜를 담을 ArrayList
+    val date_list = ArrayList<String>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,6 +36,43 @@ class MainActivity : AppCompatActivity() {
 
         setSupportActionBar(binding.mainToolbar)
         title = "메모앱"
+
+        val main_recycler_adapter = MainRecyclerAdapter()
+        binding.mainRecycler.adapter = main_recycler_adapter
+
+        binding.mainRecycler.layoutManager = LinearLayoutManager(this)
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        subject_list.clear()
+        date_list.clear()
+
+        val helper = DBHelper(this)
+
+        val sql = """
+            select memo_subject, memo_date
+            from MemoTable
+            order by memo_idx desc
+        """.trimIndent()
+
+        val c1 = helper.writableDatabase.rawQuery(sql, null)
+
+        while(c1.moveToNext()) {
+            val idx1 = c1.getColumnIndex("memo_subject")
+            val idx2 = c1.getColumnIndex("memo_date")
+
+            val memo_subject = c1.getString(idx1)
+            val memo_date = c1.getString(idx2)
+
+            //Log.d("memo_app", memo_subject)
+            //Log.d("memo_app", memo_date)
+            //Log.d("memo_app","-----------------")
+
+            subject_list.add(memo_subject)
+            date_list.add(memo_date)
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -41,5 +88,29 @@ class MainActivity : AppCompatActivity() {
             }
         }
         return super.onOptionsItemSelected(item)
+    }
+
+    inner class MainRecyclerAdapter : RecyclerView.Adapter<MainRecyclerAdapter.ViewHolderClass>() {
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolderClass {
+            val mainRecyclerBinding = MainRecyclerRowBinding.inflate(layoutInflater)
+            val holder = ViewHolderClass(mainRecyclerBinding)
+
+            return holder
+        }
+
+        override fun onBindViewHolder(holder: ViewHolderClass, position: Int) {
+            holder.rowMemoSubject.text = subject_list[position]
+            holder.rowMemoDate.text = date_list[position]
+        }
+
+        override fun getItemCount(): Int {
+            return subject_list.size
+        }
+
+        inner class ViewHolderClass(mainRecyclerBinding: MainRecyclerRowBinding) : RecyclerView.ViewHolder(mainRecyclerBinding.root){
+            val rowMemoSubject = mainRecyclerBinding.memoSubject
+            val rowMemoDate = mainRecyclerBinding.memoDate
+        }
     }
 }
